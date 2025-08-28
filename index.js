b375356f15d4e97d3da6c64b549af0d455029516
Uaychai Chotjaratwanich <13571179+uaychaich@users.noreply.github.com>
2025-08-28 12:04:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    const responseMessage = `<h2>Uay WebApp Naja</h2><p id="demo"></p><img src="https://uaystoragekiki.blob.core.windows.net/webimage/Jon_Snow_Season_8.png" alt="Jon Snow"><script type="text/javascript">var ip = location.host;document.getElementById('demo').innerHTML=ip + "<br/>" +Date();</script>`;
+    var responseMessage = `<h2>Uay WebApp Naja</h2><p id="demo"></p><img src="https://uaystoragekiki.blob.core.windows.net/webimage/Jon_Snow_Season_8.png" alt="Jon Snow"><script type="text/javascript">var ip = location.host;document.getElementById('demo').innerHTML=ip + "<br/>" +Date();</script>`;
+    if (req.query.name){responseMessage=responseMessage + "<br/><h2>Hello " + req.query.name + "</h2>";}
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage, headers: {"Content-Type": "text/html" }
